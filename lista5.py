9f7766de9cbd0982c247487fa1b33f57a296b9ad
vinicius-araujo777 <vinicius.araujo60@aluno.ce.gov.br>
2025-10-14 11:49:41 -0300

    add questão 1 da lista

---
+++ lista5.py
@@ -0,0 +1,11 @@
+#Questão 1
+# ln = []
+# for i in range(5):
+#     ln.append(input("digite o nome:"))
+# print(ln)
+# na = 0
+# for i in ln:
+#     if i [0] == "a" or i [0] == "A":
+#         na += 1
+# print(f"tem {na} nomes com a letra a")
+
